8018b2d1632d947a47a76294d0bb04739667d50c
hugo <hugohuang1111@gmail.com>
2021-09-20 00:48:45 +0800

    upgrade to 3.3.1

---
+++ assets/postprocess/PPBaseStage.ts
@@ -0,0 +1,114 @@
+
+import { _decorator, renderer, gfx, pipeline, ForwardPipeline, ForwardFlow, Material, RenderStage, PipelineStateManager, Camera } from 'cc';
+const { ccclass } = _decorator;
+
+const colors: gfx.Color[] = [new gfx.Color(0, 0, 0, 1)];
+
+const _samplerInfo = [
+    gfx.Filter.LINEAR,
+    gfx.Filter.LINEAR,
+    gfx.Filter.NONE,
+    gfx.Address.CLAMP,
+    gfx.Address.CLAMP,
+    gfx.Address.CLAMP,
+];
+
+const samplerHash = renderer.genSamplerHash(_samplerInfo);
+
+@ccclass('PPBaseStage')
+export class PPBaseStage extends RenderStage {
+
+    private _mat: Material | null = null;
+    private _ia:  gfx.InputAssembler | null = null;
+    private _framebuffer: gfx.Framebuffer | null = null;
+
+    constructor() {
+        super();
+        this._name = "PPBaseStage";
+    }
+
+    get mat() {
+        return this._mat;
+    }
+    set mat(val) {
+        this._mat = val;
+    }
+
+    get ia() {
+        return this._ia;
+    }
+    set ia(val) {
+        this._ia = val;
+    }
+
+    get framebuffer() {
+        return this._framebuffer;
+    }
+    set framebuffer(val) {
+        this._framebuffer = val;
+    }
+
+    public activate (pl: ForwardPipeline, flow: ForwardFlow) {
+        super.activate(pl, flow);
+        const device = pl.device;
+
+        const layoutInfo = new gfx.DescriptorSetLayoutInfo(pipeline.localDescriptorSetLayout.bindings);
+        const layout = device.createDescriptorSetLayout(layoutInfo);
+        const descSetInfo = new gfx.DescriptorSetInfo(layout);
+    }
+
+    public render (camera: renderer.scene.Camera): void {
+        if (camera.projectionType != Camera.ProjectionType.PERSPECTIVE) { return; }
+        if (null == this._mat) { return; }
+        if (null == this._ia) { return; }
+
+        const pl = this._pipeline;
+        const device = pl.device;
+        const cmdBuff = pl.commandBuffers[0];
+        let fb = this.framebuffer ? this.framebuffer : camera.window?.framebuffer;
+        if (null == fb) { return; }
+        const rp = fb.renderPass;
+
+        pl.pipelineUBO.updateCameraUBO(camera);
+
+        const renderArea = new gfx.Rect(0, 0, 1, 1);
+        const vp = camera.viewport;
+        renderArea.x = vp.x * camera.width;
+        renderArea.y = vp.y * camera.height;
+        renderArea.width = vp.width * camera.width * pl.pipelineSceneData.shadingScale;
+        renderArea.height = vp.height * camera.height * pl.pipelineSceneData.shadingScale;
+
+        if (camera.clearFlag & gfx.ClearFlagBit.COLOR) {
+            colors[0].x = camera.clearColor.x;
+            colors[0].y = camera.clearColor.y;
+            colors[0].z = camera.clearColor.z;
+        }
+        colors[0].w = camera.clearColor.w;
+
+        cmdBuff.beginRenderPass(rp, fb, renderArea, colors, camera.clearDepth, camera.clearStencil);
+        cmdBuff.bindDescriptorSet(pipeline.SetIndex.GLOBAL, pl.descriptorSet);
+
+        const pass = this._mat!.passes[0];
+        const shader = this._mat!.passes[0].getShaderVariant();
+
+        let inputAssembler = this._ia;
+        let pso: gfx.PipelineState | null = null;
+        if (pass != null && shader != null && inputAssembler != null) {
+            pso = PipelineStateManager.getOrCreatePipelineState(device, pass, shader, rp, inputAssembler);
+        }
+
+        const renderObjects = pl.pipelineSceneData.renderObjects;
+        if (pso != null) {
+            cmdBuff.bindPipelineState(pso);
+            cmdBuff.bindDescriptorSet(pipeline.SetIndex.MATERIAL, pass.descriptorSet);
+            cmdBuff.bindInputAssembler(inputAssembler);
+            cmdBuff.draw(inputAssembler);
+        }
+
+        cmdBuff.endRenderPass();
+    }
+
+    destroy() {
+    }
+
+}
---
+++ assets/postprocess/PPMgr.ts
@@ -0,0 +1,214 @@
+
+import { _decorator, Component, Director, ForwardFlow, ForwardStage, pipeline, ForwardPipeline, renderer, RenderPipeline, gfx, Material } from 'cc';
+import { PPBaseStage } from './PPBaseStage';
+const { ccclass, property } = _decorator;
+
+@ccclass('PPStageDesc')
+export class PPStageDesc {
+    @property(Material)
+    mat:    Material | null = null;
+}
+
+const _samplerInfo = [
+    gfx.Filter.LINEAR,
+    gfx.Filter.LINEAR,
+    gfx.Filter.NONE,
+    gfx.Address.CLAMP,
+    gfx.Address.CLAMP,
+    gfx.Address.CLAMP,
+];
+
+const samplerHash = renderer.genSamplerHash(_samplerInfo);
+
+@ccclass('PPMgr')
+export class PPMgr extends Component {
+
+    @property([PPStageDesc])
+    stageDescs: PPStageDesc[] = [];
+
+    private _framebuffer: gfx.Framebuffer | null = null;
+    private _quadIA: gfx.InputAssembler | null = null;
+
+    start () {
+        this.generatePipelineRenderData();
+        this.addPostProcessStage();
+    }
+
+    generatePipelineRenderData() {
+        const pl = Director.instance.root?.pipeline;
+        if (null == pl) {
+            return;
+        }
+        this.generateIA(pl.device);
+        this.generateFrameBuffer(pl);
+    }
+
+    addPostProcessStage() {
+        const pl = Director.instance.root?.pipeline;
+        if (null == pl) {
+            return;
+        }
+        if (!(pl instanceof ForwardPipeline)) {
+            return;
+        }
+        if (0 == this.stageDescs.length) {
+            return;
+        }
+        const fpl = pl as ForwardPipeline;
+        let flows = pl.flows;
+        if (null == flows) {
+            console.log("ERROR! can't find pileline flows");
+            return;
+        }
+        const self = this;
+        for (let flow of flows) {
+            if (flow instanceof ForwardFlow) {
+                const ff = flow as ForwardFlow;
+
+                for (let stage of ff.stages) {
+                    if (stage instanceof ForwardStage) {
+                        const fstage = stage;
+                        const originRender = fstage.render;
+
+                        fstage.render = function(camera: renderer.scene.Camera) {
+                            const originfb = camera.window?.framebuffer;
+                            if (camera.window) {
+                                camera.window._framebuffer = self._framebuffer;
+                            }
+
+                            originRender.call(fstage, camera);
+
+                            camera.window._framebuffer = originfb;
+                        }
+
+                        break;
+                    }
+                }
+
+                this.stageDescs.forEach(stageDesc => {
+                    const stage = new PPBaseStage();
+                    stage.mat = stageDesc.mat;
+                    stage.ia = this._quadIA;
+                    stage.activate(fpl, ff);
+                    ff.stages.push(stage);
+                });
+
+                break;
+            }
+        }
+    }
+
+    private generateIA(device: gfx.Device) {
+        if (null != this._quadIA) { return; }
+
+        const vbStride = Float32Array.BYTES_PER_ELEMENT * 4;
+        const vbSize = vbStride * 4;
+        const quadVB = device.createBuffer(new gfx.BufferInfo(
+            gfx.BufferUsageBit.VERTEX | gfx.BufferUsageBit.TRANSFER_DST,
+            gfx.MemoryUsageBit.HOST | gfx.MemoryUsageBit.DEVICE,
+            vbSize,
+            vbStride,
+        ));
+        const vbData = new Float32Array(4 * 4);
+        let n = 0;
+        vbData[n++] = -1.0; vbData[n++] = -1.0; vbData[n++] = 0; vbData[n++] = 1;
+        vbData[n++] = 1.0; vbData[n++] = -1.0; vbData[n++] = 1; vbData[n++] = 1;
+        vbData[n++] = -1.0; vbData[n++] = 1.0; vbData[n++] = 0; vbData[n++] = 0;
+        vbData[n++] = 1.0; vbData[n++] = 1.0; vbData[n++] = 1; vbData[n++] = 0;
+        quadVB.update(vbData);
+
+        const ibStride = Uint8Array.BYTES_PER_ELEMENT;
+        const ibSize = ibStride * 6;
+        const quadIB = device.createBuffer(new gfx.BufferInfo(
+            gfx.BufferUsageBit.INDEX | gfx.BufferUsageBit.TRANSFER_DST,
+            gfx.MemoryUsageBit.HOST | gfx.MemoryUsageBit.DEVICE,
+            ibSize,
+            ibStride,
+        ));
+        const indices = new Uint8Array(6);
+        indices[0] = 0; indices[1] = 1; indices[2] = 2;
+        indices[3] = 1; indices[4] = 3; indices[5] = 2;
+        quadIB.update(indices);
+
+        const attributes = new Array<gfx.Attribute>(2);
+        attributes[0] = new gfx.Attribute('a_position', gfx.Format.RG32F);
+        attributes[1] = new gfx.Attribute('a_texCoord', gfx.Format.RG32F);
+
+        const quadIA = device.createInputAssembler(new gfx.InputAssemblerInfo(
+            attributes,
+            [quadVB],
+            quadIB,
+        ));
+
+        this._quadIA = quadIA;
+    }
+
+    private generateFrameBuffer (pl: RenderPipeline) {
+        if (null != this._framebuffer) { return; }
+
+        const device = pl.device;
+        if (null == device) {
+            return;
+        }
+        const width = device.width;
+        const height = device.height;
+
+        const colorAttachment0 = new gfx.ColorAttachment();
+        colorAttachment0.format = gfx.Format.RGBA16F;
+        colorAttachment0.loadOp = gfx.LoadOp.CLEAR;
+        colorAttachment0.storeOp = gfx.StoreOp.STORE;
+        const depthStencilAttachment = new gfx.DepthStencilAttachment();
+        depthStencilAttachment.format = device.depthStencilFormat;
+        depthStencilAttachment.depthLoadOp = gfx.LoadOp.CLEAR;
+        depthStencilAttachment.depthStoreOp = gfx.StoreOp.STORE;
+        depthStencilAttachment.stencilLoadOp = gfx.LoadOp.CLEAR;
+        depthStencilAttachment.stencilStoreOp = gfx.StoreOp.STORE;
+        const rPass = device.createRenderPass(new gfx.RenderPassInfo([colorAttachment0], depthStencilAttachment));
+
+        const clrTexs : gfx.Texture[] = [];
+        clrTexs.push(device.createTexture(new gfx.TextureInfo(
+            gfx.TextureType.TEX2D,
+            gfx.TextureUsageBit.COLOR_ATTACHMENT | gfx.TextureUsageBit.SAMPLED,
+            gfx.Format.RGBA16F,
+            width,
+            height,
+        )));
+
+        const depthTex: gfx.Texture = device.createTexture(new gfx.TextureInfo(
+            gfx.TextureType.TEX2D,
+            gfx.TextureUsageBit.DEPTH_STENCIL_ATTACHMENT,
+            device.depthStencilFormat,
+            width,
+            height,
+        ));
+
+        const fb: gfx.Framebuffer = device.createFramebuffer(new gfx.FramebufferInfo(
+            rPass,
+            clrTexs,
+            depthTex,
+        ));
+
+        const descriptorSet = pl.descriptorSet;
+        descriptorSet?.bindTexture(pipeline.UNIFORM_GBUFFER_ALBEDOMAP_BINDING, fb.colorTextures[0]!);
+
+        const sampler = renderer.samplerLib.getSampler(device, samplerHash);
+        descriptorSet?.bindSampler(pipeline.UNIFORM_GBUFFER_ALBEDOMAP_BINDING, sampler);
+
+        this._framebuffer = fb;
+    }
+
+    protected destroyQuadInputAssembler () {
+        if (this._quadIA) {
+            let vbs = this._quadIA?.vertexBuffers;
+            vbs.forEach(vb => {
+                vb.destroy();
+            });
+            this._quadIA.indexBuffer?.destroy();
+            this._quadIA.destroy();
+            this._quadIA = null;
+        }
+
+    }
+
+}
+
